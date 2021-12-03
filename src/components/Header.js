6090eaebfeb4628ac55b521fe7f86d936bee5aef
koola123 <82808273+koola123@users.noreply.github.com>
2021-12-03 02:21:31 +0100

    Removed location

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,12 @@
 import PropTypes from 'prop-types'
 import Button from './Button'
-import { useLocation } from 'react-router-dom'
 
 const Header = ({ title, onAdd, showAdd }) => {
-  const location = useLocation()
 
   return (
     <header className="header">
       <h1 style={{ padding: '10px', borderBottom: "5px solid gold" }}>{title}</h1>
-      {location.pathname === "/" && <Button color={showAdd ? "gold" : "#fff"} text={showAdd ? "Close" : "Add"} onClick={onAdd} />}
+      <Button color={showAdd ? "gold" : "#fff"} text={showAdd ? "Close" : "Add"} onClick={onAdd} />
     </header>
   )
 }
